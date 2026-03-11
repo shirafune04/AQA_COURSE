57ccafa1c46de5c1b790f2f66f672f44568b4fda
shirafune04 <chanshirafune@gmail.com>
2026-03-11 11:48:55 +0300

    Lesson7junit5

--- src/test/java/ArithmeticCalculatorTest.java
+++ src/test/java/ArithmeticCalculatorTest.java
@@ -2,42 +2,50 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 class ArithmeticCalculatorTest {
     private ArithmeticCalculator calculator;
 
     @BeforeEach
-    void setUp () {
+    void setUp() {
         calculator = new ArithmeticCalculator();
     }
+
     @Test
-    void testAdd(){
-        assertEquals(calculator.add(3,5),8);
-        assertEquals(calculator.add(20,15),35);
-        assertEquals(calculator.add(2,2),4);
+    void testAdd() {
+        assertEquals(8, calculator.add(3, 5));
+        assertEquals(35, calculator.add(20, 15));
+        assertEquals(4, calculator.add(2, 2));
     }
+
     @Test
-    void testSubtract(){
-        assertEquals(calculator.subtract(9,8),1);
-        assertEquals(calculator.subtract(2,2),0);
-        assertEquals(calculator.subtract(4,1),3);
+    void testSubtract() {
+        assertEquals(1, calculator.subtract(9, 8));
+        assertEquals(0, calculator.subtract(2, 2));
+        assertEquals(3, calculator.subtract(4, 1));
     }
+
     @Test
-    void testMultiply(){
-        assertEquals(calculator.multiply(2,2),4);
-        assertEquals(calculator.multiply(8,9),72);
-        assertEquals(calculator.multiply(5,1),5);
+    void testMultiply() {
+        assertEquals(4, calculator.multiply(2, 2));
+        assertEquals(72, calculator.multiply(8, 9));
+        assertEquals(5, calculator.multiply(5, 1));
     }
+
     @Test
-    void testDivide(){
-        assertEquals(calculator.divide(2,2),1);
-        assertEquals(calculator.divide(9,3),3);
-        assertEquals(calculator.divide(10,5),2);
+    void testDivide() {
+        assertEquals(1, calculator.divide(2, 2));
+        assertEquals(3, calculator.divide(9, 3));
+        assertEquals(2, calculator.divide(10, 5));
     }
+
     @Test
     void testDivideByZero() {
         assertThrows(ArithmeticException.class, () -> {
-            calculator.divide(7,0);
+            calculator.divide(7, 0);
         });
     }
-
 }
--- src/test/java/NumberComparatorTest.java
+++ src/test/java/NumberComparatorTest.java
@@ -9,22 +9,22 @@ class NumberComparatorTest {
     void setUp(){
         comparator = new NumberComparator();
     }
+
     @Test
-    void testFirstLessThanSecond (){
-        assertEquals(comparator.compare(1,5),-1);
-        assertEquals(comparator.compare(6,9),-1);
-    }
-    @Test
-    void testFirstGreaterThanSecond (){
-        assertEquals(comparator.compare(3,1),1);
-        assertEquals(comparator.compare(12,5),1);
-    }
-    @Test
-    void testFirstEqualSecond (){
-        assertEquals(comparator.compare(6,6),0);
-        assertEquals(comparator.compare(2,2),0);
+    void testFirstLessThanSecond() {
+        assertEquals(-1, comparator.compare(1,5));
+        assertEquals(-1, comparator.compare(6,9));
     }
 
+    @Test
+    void testFirstGreaterThanSecond() {
+        assertEquals(1, comparator.compare(3,1));
+        assertEquals(1, comparator.compare(12,5));
+    }
 
-
+    @Test
+    void testFirstEqualSecond() {
+        assertEquals(0, comparator.compare(6,6));
+        assertEquals(0, comparator.compare(2,2));
+    }
 }
